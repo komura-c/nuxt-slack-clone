1f0e315374ad0d04640d1d2ec46431b90f801075
komura <affirman03139@outlook.jp>
2020-10-29 02:59:59 +0900

    feat(markup): 画面のマークアップを実装

---
+++ components/Messages.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="chats-container">
+    <message />
+  </div>
+</template>
+
+<script>
+import Message from "~/components/Message.vue";
+export default {
+  components: {
+    Message
+  }
+};
+</script>
+
+<style scoped>
+.chats-container {
+  padding: 16px;
+}
+</style>
